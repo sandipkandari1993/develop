8533d71eb5e36dec8d8d5705deafd7fa1ae1a3df
sandip <sandip.kandari93@gmail.com>
2018-10-18 13:04:38 +0530

    added new test class for fli]pkart test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,17 @@
 public class Test {
 
+    TestFlip obj1;
+
     public static  void  main(String a[])
     {
         System.out.print("commiting git changes");
+        Test t= new Test();
+        t.validatetest();
+    }
+
+    public void validatetest()
+    {
+        obj1= new TestFlip();
+        obj1.testLogin("sandip","admin");
     }
 }
---
+++ src/test/java/TestFlip.java
@@ -0,0 +1,8 @@
+public class TestFlip {
+
+    public void testLogin(String userid,String pwd)
+    {
+        System.out.print("user ID "+ userid);
+        System.out.print("PWD IS  "+ pwd);
+    }
+}
